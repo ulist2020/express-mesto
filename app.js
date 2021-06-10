2f6552c9dbf833178d756476d0a436026628c988
ulist2020 <75067604+ulist2020@users.noreply.github.com>
2021-06-10 10:21:42 +0500

    fix: подключится к Mongo + создать user.js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
 
 app.listen(PORT, () => {
     // Если всё работает, консоль покажет, какой порт приложение слушает
