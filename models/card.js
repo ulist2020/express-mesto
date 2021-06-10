185e806d812dfc59f6ee173caebde555e96a02af
ulist2020 <75067604+ulist2020@users.noreply.github.com>
2021-06-10 10:30:44 +0500

    fix: создать card.js

---
+++ models/card.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  owner: {
+    type: { driver: mongoose.ObjectId },
+    required: true,
+  },
+  likes: {
+    type: [{ driver: mongoose.ObjectId }],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
